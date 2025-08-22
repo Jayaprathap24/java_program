b6020ca7d5d0cd4d9eaa8e434763667e068fbcca
R.JAYAPRATHAP <jayaprathap561@gmail.com>
2025-08-22 15:04:52 +0530

    Add files via upload

---
+++ Dailytask.java
@@ -0,0 +1,171 @@
+package neww;
+
+public class Dailytask {
+
+	public static void main(String[] args) {
+//		int a=9;
+//		if(a%2==0)//odd even number check using if statement 
+//		{
+//			System.out.println("the number is even");
+//			}
+//		
+//		
+//		if(a%2!=0)
+//			{
+//				System.out.println("the number is odd");
+//			}
+		
+		
+//		int number=10; //reverse number using if statement
+//		
+//			if(number<=10) {
+//				System.out.println(number);
+//				number--;
+//			}
+//			if(number<=10) {
+//				System.out.println(number);
+//				number--;
+//			}
+//			if(number<=10) {
+//				System.out.println(number);
+//				number--;
+//			}
+//			if(number<=10) {
+//				System.out.println(number);
+//				number--;
+//			}
+//			if(number<=10) {
+//				System.out.println(number);
+//				number--;
+//			}
+//			if(number<=10) {
+//				System.out.println(number);
+//				number--;
+//			}
+//			if(number<=10) {
+//				System.out.println(number);
+//				number--;
+//			}
+//			if(number<=10) {
+//				System.out.println(number);
+//				number--;
+//			}
+//			if(number<=10) {
+//				System.out.println(number);
+//				number--;
+//			}
+//		if(number<=10) {
+//			System.out.println(number);
+//			number--;
+//		}
+//	
+		
+		
+//		int num=0;// check the number is positive negative or zero using if statement
+//		if(num>0) {
+//			System.out.println("popsitive number");
+//		}
+//		if(num<0) {
+//			System.out.println("negative number");
+//		}
+//		if(num==0) {
+//			System.out.println("the number is zero");
+//		}
+		
+//		int year=2002;//leap year using if statement
+//		if (year%400==0 || year%4==0 && year%100 !=0) {
+//			System.out.println("its leap year");
+//		}
+		
+//		int a=13;
+//		if(a%2==0)//odd even number check using if else statement 
+//		{
+//			System.out.println("the number is even");
+//		}
+//		else {
+//			System.out.println("the number is odd even");
+//		}
+//		
+//		int number=12; //reverse number using if else statement
+//	
+//			if(number<=10) {
+//				System.out.println(number);
+//				number--;
+//			}
+//			if(number<=10) {
+//				System.out.println(number);
+//				number--;
+//			}
+//		   if(number<=10) {
+//				System.out.println(number);
+//				number--;
+//			}
+//		 if(number<=10) {
+//				System.out.println(number);
+//				number--;
+//			}
+//		  if(number<=10) {
+//				System.out.println(number);
+//				number--;
+//			}
+//			if(number<=10) {
+//				System.out.println(number);
+//				number--;
+//			}
+//			 if(number<=10) {
+//				System.out.println(number);
+//				number--;
+//			}
+//			 if(number<=10) {
+//				System.out.println(number);
+//				number--;
+//			}
+//			 if(number<=10) {
+//				System.out.println(number);
+//				number--;
+//			}
+//			 if(number<=10) {
+//					System.out.println(number);
+//					number--;
+//				}
+//			 else {
+//				 System.out.println("the number in unvalid");
+//			 }
+//		
+		
+//		int num=0;// check the number is positive negative or zero using if else statement
+//		if(num>0) {
+//			System.out.println("popsitive number");
+//		}
+//		if(num<0) {
+//			System.out.println("negative number");
+//		}
+//		else {
+//			System.out.println("the number is zero");
+//		}
+		
+//		int year=20024;//leap year using if else statement
+//		if (year%400==0 || year%4==0 && year%100 !=0) {
+//			System.out.println("its leap year");
+//		}
+//		else {
+//			System.out.println("its not leap year");
+//			
+//		}
+		 int number=6;
+		 if(number%1==0 && number%number==0 || number/number==0) {
+			 System.out.println("its prime number"); 
+		 }
+		 else {
+			 System.out.println("its not prime number");
+		 }
+		
+			
+		}
+		
+		
+		
+		
+	}
+
+	
